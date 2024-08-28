0fc45e7dcf827074fd884dc2a17f55870f447732
khubetsov.ay <khubetsov.ay@tr-media.ru>
2024-08-28 11:30:16 +0300

    practice: Compose basics

--- app/src/main/java/com/example/test/MainActivity.kt
+++ app/src/main/java/com/example/test/MainActivity.kt
@@ -4,25 +4,27 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.collection.mutableLongLongMapOf
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.AbsoluteAlignment
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -39,9 +41,12 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting(message = stringResource(R.string.message_string_to_screen),
-                        from = stringResource(R.string.from_who_message),
-                        modifier = Modifier.padding(8.dp))
+                    ComposeQuadrantApp()
+//                    TaskCompletedScreen()
+//                    Greeting(firstText = stringResource(R.string.first_text_message),
+//                        secondText = stringResource(R.string.second_text_message),
+//                        thirdText = stringResource(R.string.third_text_message),
+//                        modifier = Modifier.padding(8.dp))
                 }
             }
         }
@@ -49,38 +54,118 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(message: String, from: String, modifier: Modifier = Modifier) {
-    val image = painterResource(R.drawable.androidparty)
+fun Greeting(firstText: String, secondText: String, thirdText : String, modifier: Modifier = Modifier) {
+    val image = painterResource(R.drawable.bg_compose_background)
     Box (modifier) {
-        Image(painter = image, contentDescription = null,
-            contentScale = ContentScale.Crop,
-            alpha = 0.5F)
+        Column (
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally) {
+            Image(painter = image, contentDescription = null,
+                contentScale = ContentScale.FillWidth,
+                alpha = 0.9F)
+            Text(
+                text = firstText,
+                textAlign = TextAlign.Justify,
+                fontSize = 24.sp,
+                modifier = Modifier
+                    .padding(16.dp)
+            )
+            Text(
+                text = secondText,
+                modifier = Modifier
+                    .padding(16.dp)
+            )
+            Text(
+                text = thirdText,
+                textAlign = TextAlign.Justify,
+                modifier = Modifier
+                    .padding(16.dp)
+            )
+        }
     }
-    Column (
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally,
-        modifier = modifier.padding(8.dp)) {
-        Text(
-            text = message,
-            textAlign = TextAlign.Justify,
-            fontSize = 20.sp,
-            lineHeight = 50.sp,
-        )
-        Text(
-            text = from,
-            fontSize = 25.sp,
-            lineHeight = 50.sp,
+}
+@Composable
+fun TaskCompletedScreen() {
+    Box {
+        Column (
             modifier = Modifier
-                .padding(8.dp)
-                .align(alignment = Alignment.Start)
-        )
+                .fillMaxWidth()
+                .fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally) {
+            val image = painterResource(R.drawable.ic_task_completed)
+            Image(painter = image, contentDescription = null,
+                contentScale = ContentScale.FillWidth,
+                alpha = 0.9F)
+            Text(
+                text = stringResource(R.string.task_completed_message),
+                fontWeight = FontWeight.Bold,
+                modifier = Modifier.padding(top = 24.dp, bottom = 8.dp))
+            Text(
+                text = stringResource(R.string.task_completed_nice_work_message),
+                fontSize = 16.sp
+            )
+        }
     }
 }
 
-@Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    TestTheme {
-        Greeting("1 row 1 column","1 row 2 column")
+fun ComposeQuadrantApp() {
+    Column(Modifier.fillMaxWidth()) {
+        Row(Modifier.weight(1f)) {
+            ComposableInfoCard(
+                title = stringResource(R.string.first_title),
+                description = stringResource(R.string.first_description),
+                backgroundColor = Color(0xFFEADDFF),
+                modifier = Modifier.weight(1f)
+            )
+            ComposableInfoCard(
+                title = stringResource(R.string.second_title),
+                description = stringResource(R.string.second_description),
+                backgroundColor = Color(0xFFD0BCFF),
+                modifier = Modifier.weight(1f)
+            )
+        }
+        Row(Modifier.weight(1f)) {
+            ComposableInfoCard(
+                title = stringResource(R.string.third_title),
+                description = stringResource(R.string.third_description),
+                backgroundColor = Color(0xFFB69DF8),
+                modifier = Modifier.weight(1f)
+            )
+            ComposableInfoCard(
+                title = stringResource(R.string.fourth_title),
+                description = stringResource(R.string.fourth_description),
+                backgroundColor = Color(0xFFF6EDFF),
+                modifier = Modifier.weight(1f)
+            )
+        }
+    }
+}
+
+@Composable
+private fun ComposableInfoCard(
+    title: String,
+    description: String,
+    backgroundColor: Color,
+    modifier: Modifier = Modifier
+) {
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .background(backgroundColor)
+            .padding(16.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = title,
+            modifier = Modifier.padding(bottom = 16.dp),
+            fontWeight = FontWeight.Bold
+        )
+        Text(
+            text = description,
+            textAlign = TextAlign.Justify
+        )
     }
 }
